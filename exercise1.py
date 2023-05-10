8b13d88463402ff661f2d469b2071cf973fbe816
Carolina <kgarciajuarez@gmail.com>
2023-05-10 00:04:19 -0600

    agregar código ejercicio

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+numero = input('Digite un número: ')
+print(numero)
+print(type(numero))
+
+numeroentero = int (numero)
+print(type(numeroentero))
+
+numero2 = numeroentero / 3
+print('Número 2: ', numero2)
+
+print(numero2 > 2)
+
+
